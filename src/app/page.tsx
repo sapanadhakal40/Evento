ac7391b789596913a1a5022db615f866fbdd3ca9
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-07 01:59:24 +0300

    add homepage UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,30 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center px-3 ">
-      <h1>Find events around you</h1>
-      <p>Browse more than 10,000 events around you</p>
+    <main className="flex flex-col items-center px-3 pt-36">
+      <h1 className="text-3xl lg:text-6xl font-bold tracking-tight">
+        Find events around you
+      </h1>
+      <p className="mb-12 mt-7 text-2xl lg:text-3xl">
+        Browse more than{" "}
+        <span className="font-bold italic underline text-accent">
+          {" "}
+          10,000 events{" "}
+        </span>
+        around you
+      </p>
 
-      <form>
-        <input placeholder="Search events" spellCheck={false} />
+      <form className="w-full sm:w-[580px]">
+        <input
+          className=" w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-accent/50 transition focus:ring-2 focus:bg-white/10"
+          placeholder="Search events"
+          spellCheck={false}
+        />
       </form>
 
-      <section>
+      <section className="mt-4 flex gap-x-4 text-sm text-white/50">
         <p>Popular:</p>
-        <div>
+        <div className="space-x-2 font-semibold">
           <Link href="events/austin">Austin</Link>
           <Link href="events/seattle">Seattle</Link>
         </div>
