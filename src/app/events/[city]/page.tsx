15d3a32768159d80779d5f00b922619bd0894e70
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-07 16:54:13 +0300

    use city name in url to fetch correct event

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -7,12 +7,12 @@ type EventsPageProps = {
   };
 };
 export default async function EventsPage({ params }: EventsPageProps) {
+  const city = params.city;
   const response = await fetch(
-    "https://bytegrad.com/course-assets/projects/evento/api/events?city=austin"
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
   );
   const events: EventoEvent[] = await response.json();
   // console.log(events);
-  const city = params.city;
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
