8793fd29ededc6fcef190fd8a4903f8b77481fd7
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-07 21:40:52 +0300

    Add event detail page and section components for event page layout

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,3 +1,84 @@
-export default function Eventpage() {
-  return <div>Event Page</div>;
+import H1 from "@/components/h1";
+import Image from "next/image";
+
+type EventPageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function Eventpage({ params }: EventPageProps) {
+  const slug = params.slug;
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
+  );
+  const event = await response.json();
+  // console.log(event);
+  return (
+    <main>
+      <section className="relative overflow-hidden flex justify-center items-center py-14 md:py-20">
+        <Image
+          src={event.imageUrl}
+          className="object-cover z-0 blur-3xl"
+          alt="Event background image"
+          fill
+          quality={50}
+          priority
+          sizes="(max-width:1280px) 100vw ,1280px"
+        />
+
+        <div className="z-1 relative flex flex-col lg:flex-row gap-6 lg:gap-16 mx-4 md:mx-8">
+          <Image
+            src={event.imageUrl}
+            alt={event.name}
+            width={300}
+            height={201}
+            className="rounded-xl border-2 border-white/50 object-cover mx-auto lg:mx-0"
+          />
+
+          <div className="flex flex-col mx-4 md:mx-8 lg:mx-0 space-y-4">
+            <p className=" text-white/75">
+              {/* display date : day ,month name and day num*/}
+              {new Date(event.date).toLocaleDateString("en-us", {
+                weekday: "long",
+                month: "long",
+                day: "numeric",
+              })}
+            </p>
+            <H1 className="whitespace-nowrap lg:text-5xl">{event.name}</H1>
+            <p className="whitespace-nowrap text-xl text-white/75">
+              Organized by <span className="italic">{event.organizerName}</span>
+            </p>
+            <button className="bg-white/20 text-lg capitalize bg-blur mt-4 lg:mt-auto w-[95vw] rounded-md border-white/10 border-2 sm:w-full py-2 state-effects">
+              Get tickets
+            </button>
+          </div>
+        </div>
+      </section>
+
+      <div className="text-center min-h-[75vh] px-5 py-16">
+        <Section>
+          <SectionHeading>About this event</SectionHeading>
+          <SectionContent>{event.description}</SectionContent>
+        </Section>
+        <Section>
+          <SectionHeading>Location</SectionHeading>
+
+          <SectionContent> {event.location}</SectionContent>
+        </Section>
+      </div>
+    </main>
+  );
+}
+function Section({ children }: { children: React.ReactNode }) {
+  return <section className="mb-12">{children}</section>;
+}
+function SectionHeading({ children }: { children: React.ReactNode }) {
+  return <h2 className="text-2xl mb-8">{children}</h2>;
+}
+
+function SectionContent({ children }: { children: React.ReactNode }) {
+  return (
+    <p className="max-w-4xl mx-auto text-lg leading-8 tracking">{children}</p>
+  );
 }
