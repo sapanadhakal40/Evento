237a7f74ad6dc25e5c639be07e46aa05b3dd1aec
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-07 13:12:31 +0300

    Add event date in event card component

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -6,7 +6,7 @@ type EventCardProps = {
 };
 export default function EventCard({ event }: EventCardProps) {
   return (
-    <section className="flex flex-col flex-1 basis-80 h-[400px] max-w-[500px] bg-white/[2%] rounded-xl overflow-hidden">
+    <section className="relative flex flex-col flex-1 basis-80 h-[400px] max-w-[500px] bg-white/[2%] rounded-xl overflow-hidden">
       <Image
         src={event.imageUrl}
         alt={event.name}
@@ -20,6 +20,20 @@ export default function EventCard({ event }: EventCardProps) {
         <p className="italic text-white/75">By {event.organizerName}</p>
         <p className="mt-4 text-sm text-white/50 ">{event.location}</p>
       </div>
+
+      <section className="absolute flex flex-col justify-center items-center top-[12px] left-[12px] h-[45px] w-[45px] bg-black/30 rounded-md">
+        <p className="text-xl font-bold -mb-[5px]">
+          {/* {new Date(event.date).getDate()} */}
+          {new Date(event.date).toLocaleDateString("en-US", {
+            day: "2-digit",
+          })}
+        </p>
+        <p className="text-xs uppercase text-accent">
+          {new Date(event.date).toLocaleDateString("en-US", {
+            month: "short",
+          })}
+        </p>
+      </section>
     </section>
   );
 }
