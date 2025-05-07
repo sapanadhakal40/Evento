33e69e74105d25d8bb3d34eb68fcfe866df4e06c
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-07 09:47:14 +0300

    make logo clickable with link

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Logo() {
   return (
-    <Image
-      src="https://bytegrad.com/course-assets/react-nextjs/evento.png"
-      alt="Evento Logo"
-      width={50}
-      height={12}
-    />
+    <Link href="/">
+      <Image
+        src="https://bytegrad.com/course-assets/react-nextjs/evento.png"
+        alt="Evento Logo"
+        width={50}
+        height={12}
+      />
+    </Link>
   );
 }
