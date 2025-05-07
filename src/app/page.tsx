81a33acfe90e7d82e2fbb7fe78f677b4fa496d22
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-07 16:16:25 +0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-3 pt-36">
-      <H1>Find events around you</H1>
+      <H1 className="text-4xl">Find events around you</H1>
       <p className="mb-12 mt-7 text-2xl lg:text-3xl">
         Browse more than{" "}
         <span className="font-bold italic underline text-accent">
