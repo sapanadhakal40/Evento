cd14aab8a3f26f29564e3dc08a225a9c1d05d830
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-07 10:47:07 +0300

    update scroll style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-900 text-white`}>
+      <body
+        className={`${inter.className} bg-gray-900 text-white overflow-y-scroll`}
+      >
         <Container>
           <Header />
           {children}
