60601285bb2a6bb01aaaa01de7103565c807c363
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-07 09:27:20 +0300

    Add reusable H1 component

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,3 +1,8 @@
+import H1 from "@/components/h1";
 export default function EventsPage() {
-  return <div>Events Page</div>;
+  return (
+    <main>
+      <H1>Events in Austin</H1>
+    </main>
+  );
 }
