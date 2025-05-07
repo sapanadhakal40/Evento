6519c7570857ab79dc10871ad3375877763d00ee
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-07 12:52:09 +0300

    add responsive styling

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -1,8 +1,25 @@
 import { EventoEvent } from "@/lib/types";
+import Image from "next/image";
 
 type EventCardProps = {
   event: EventoEvent;
 };
 export default function EventCard({ event }: EventCardProps) {
-  return <section>{event.name}</section>;
+  return (
+    <section className="flex flex-col flex-1 basis-80 h-[400px] max-w-[500px] bg-white/[2%] rounded-xl overflow-hidden">
+      <Image
+        src={event.imageUrl}
+        alt={event.name}
+        width={500}
+        height={300}
+        className="h-[50%] object-fit rounded-lg overflow-hidden"
+      />
+
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <h2 className="text-2xl font-semibold"> {event.name}</h2>
+        <p className="italic text-white/75">By {event.organizerName}</p>
+        <p className="mt-4 text-sm text-white/50 ">{event.location}</p>
+      </div>
+    </section>
+  );
 }
--- src/components/event-list.tsx
+++ src/components/event-list.tsx
@@ -6,7 +6,7 @@ type EventsListProps = {
 };
 export default function EventsList({ events }: EventsListProps) {
   return (
-    <section>
+    <section className="max-w-[1100px] flex flex-wrap gap-10 justify-center px-20">
       {events.map((event) => (
         <EventCard key={event.id} event={event} />
       ))}
