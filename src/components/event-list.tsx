edbe87983397288fe2446e919da32832ddf8cd06
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-08 16:35:43 +0300

    Move events fetching logic to event-list component

--- src/components/event-list.tsx
+++ src/components/event-list.tsx
@@ -1,10 +1,18 @@
 import { EventoEvent } from "@/lib/types";
 import EventCard from "./event-card";
+// import { sleep } from "@/lib/utils";
 
 type EventsListProps = {
-  events: EventoEvent[];
+  city: string;
 };
-export default function EventsList({ events }: EventsListProps) {
+
+export default async function EventsList({ city }: EventsListProps) {
+  // await sleep(2000);
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
+  );
+  const events: EventoEvent[] = await response.json();
+  // console.log(events);
   return (
     <section className="max-w-[1100px] flex flex-wrap gap-10 justify-center px-[20px] relative">
       {events.map((event) => (
