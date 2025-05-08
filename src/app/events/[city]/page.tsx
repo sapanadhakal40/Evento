8be67d1eb54d96923e0014ed1bee7f75ffd91efe
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-08 16:37:07 +0300

    Add suspense boundary with loading

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,7 +1,7 @@
 import EventsList from "@/components/event-list";
 import H1 from "@/components/h1";
-import { EventoEvent } from "@/lib/types";
-import { sleep } from "@/lib/utils";
+import { Suspense } from "react";
+import Loading from "./loading";
 type EventsPageProps = {
   params: {
     city: string;
@@ -10,13 +10,6 @@ type EventsPageProps = {
 export default async function EventsPage({ params }: EventsPageProps) {
   const city = params.city;
 
-  await sleep(2000);
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
-  );
-  const events: EventoEvent[] = await response.json();
-  // console.log(events);
-
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
       <H1 className="mb-28">
@@ -24,8 +17,9 @@ export default async function EventsPage({ params }: EventsPageProps) {
         {city !== "all" &&
           `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`}
       </H1>
-
-      <EventsList events={events} />
+      <Suspense fallback={<Loading />}>
+        <EventsList city={city} />
+      </Suspense>
     </main>
   );
 }
