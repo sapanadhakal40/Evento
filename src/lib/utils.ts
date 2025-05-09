78ed2c22977d063b600a1a1ad9e3dc53fdf566e7
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-09 11:13:52 +0300

    Add capitalize utility function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,6 +5,10 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+export function capitalize(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 export async function sleep(ms: number) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
