1fa5346c279bdff8a1702cf681c7597dd82bd130
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-09 17:05:36 +0300

    Add paginationcontrols props, type safety and conditional rendering for previous/next buttons

--- src/components/pagination-controls.tsx
+++ src/components/pagination-controls.tsx
@@ -1,19 +1,32 @@
 import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 
+type PaginationControlsProps = {
+  previousPath: string;
+  nextPath: string;
+};
 const btnStyles =
   'className="flex items-center gap-x-2 text-white px-5 py-3 bg-white/5 rounded-md opacity-75 hover:opacity-100 transition text-sm"';
-export default function PaginationControls() {
+export default function PaginationControls({
+  previousPath,
+  nextPath,
+}: PaginationControlsProps) {
   return (
     <section className="flex justify-between w-full">
-      <Link href="/events/austin?page=1" className={btnStyles}>
-        <ArrowLeftIcon />
-        Previous
-      </Link>
-      <Link href="/events/austin?page=3" className={btnStyles}>
-        <ArrowRightIcon />
-        Next
-      </Link>
+      {previousPath ? (
+        <Link href={previousPath} className={btnStyles}>
+          <ArrowLeftIcon />
+          Previous
+        </Link>
+      ) : (
+        <div />
+      )}
+      {nextPath && (
+        <Link href={nextPath} className={btnStyles}>
+          Next
+          <ArrowRightIcon />
+        </Link>
+      )}
     </section>
   );
 }
