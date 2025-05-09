f8616fe73320547ad4918ed688824189db97a825
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-09 12:28:59 +0300

    Move data fetching logic to utils and implement in event list and event detail page

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import H1 from "@/components/h1";
+import { getEvent } from "@/lib/utils";
 import { Metadata } from "next";
 import Image from "next/image";
 
@@ -10,11 +11,8 @@ type Props = {
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const slug = params.slug;
+  const event = await getEvent(slug);
 
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
-  );
-  const event = await response.json();
   return {
     title: event.name,
   };
@@ -22,10 +20,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
 export default async function Eventpage({ params }: Props) {
   const slug = params.slug;
-  const response = await fetch(
-    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
-  );
-  const event = await response.json();
+  const event = await getEvent(slug);
   // console.log(event);
   return (
     <main>
