b86db424fa9550c59f85ab7cfaf689bc8db2e051
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-09 15:30:43 +0300

    Implement search params for pagination

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -10,6 +10,9 @@ type Props = {
     city: string;
   };
 };
+type EventsPageProps = Props & {
+  searchParams: { [key: string]: string[] | undefined };
+};
 
 export function generateMetadata({ params }: Props) {
   const city = params.city;
@@ -19,8 +22,12 @@ export function generateMetadata({ params }: Props) {
   };
 }
 
-export default async function EventsPage({ params }: Props) {
+export default async function EventsPage({
+  params,
+  searchParams,
+}: EventsPageProps) {
   const city = params.city;
+  const page = searchParams.page || 1; // || undefined or 1 ,??- undefined or null then page 1
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
@@ -29,7 +36,7 @@ export default async function EventsPage({ params }: Props) {
         {city !== "all" && `Events in ${capitalize(city)}`}
       </H1>
       <Suspense fallback={<Loading />}>
-        <EventsList city={city} />
+        <EventsList city={city} page={+page} />
       </Suspense>
     </main>
   );
