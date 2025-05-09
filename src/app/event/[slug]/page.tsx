796c506705c2b7cda2b3a21c91bce6f2d60cf989
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-05-09 11:17:42 +0300

    Generate metadata for event detail page and fetch event data to use event name

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,13 +1,26 @@
 import H1 from "@/components/h1";
+import { Metadata } from "next";
 import Image from "next/image";
 
-type EventPageProps = {
+type Props = {
   params: {
     slug: string;
   };
 };
 
-export default async function Eventpage({ params }: EventPageProps) {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const slug = params.slug;
+
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
+  );
+  const event = await response.json();
+  return {
+    title: event.name,
+  };
+}
+
+export default async function Eventpage({ params }: Props) {
   const slug = params.slug;
   const response = await fetch(
     `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
